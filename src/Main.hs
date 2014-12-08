c82d19fe441cba4d796824356a0b8f24875ab957
Haukur Rosinkranz <hauxir@gmail.com>
2014-12-08 16:30:35 +0100

    demonstration

--- src/Main.hs
+++ src/Main.hs
@@ -1,15 +1,66 @@
 import UI.NCurses
+import TetrisUI.Grid
+import TetrisGame.Logic
+
+gridX = 50
+gridY = 4
+
+drawblock 'O' = do
+                setColor defaultColorID
+                drawString block
+
+drawblock 'I' = do
+                setColor defaultColorID
+                drawString block
+
+drawblock 'S' = do
+                setColor defaultColorID
+                drawString block
+
+drawblock 'T' = do
+                setColor defaultColorID
+                drawString block
 
 main :: IO ()
 main = runCurses $ do
+    gridcolor <- gridColor
+    red <- newColorID ColorRed ColorRed 2
+    green <- newColorID ColorGreen ColorGreen 3
+    blue <- newColorID ColorBlue ColorBlue 4
+    yellow <- newColorID ColorYellow ColorYellow 5
+    setCursorMode CursorInvisible
     setEcho False
     w <- defaultWindow
     updateWindow w $ do
-        moveCursor 1 10
-        drawString "Hello world!"
-        moveCursor 3 10
-        drawString "(press q to quit)"
-        moveCursor 0 0
+
+        drawGrid gridY gridX gridcolor
+        moveCursor (gridLines+gridY) (gridX+2)
+
+        --I block
+        setColor red
+        drawString (blocks 4)
+
+        --O block
+        setColor blue
+        moveCursor (gridLines+gridY-1) (gridX+2)
+        drawString (blocks 2)
+        moveCursor (gridLines+gridY-2) (gridX+2)
+        drawString (blocks 2)
+
+        --S block
+        setColor green
+        moveCursor (gridLines+gridY) (gridX+8)
+        drawString (blocks 2)
+        moveCursor (gridLines+gridY-1) (gridX+10)
+        drawString (blocks 2)
+
+        --T block
+        setColor yellow
+        moveCursor (gridLines+gridY) (gridX+14)
+        drawString (blocks 3)
+        moveCursor (gridLines+gridY-1) (gridX+16)
+        drawString (blocks 1)
+
     render
     waitFor w (\ev -> ev == EventCharacter 'q' || ev == EventCharacter 'Q')
 
---
+++ src/TetrisUI/Grid.hs
@@ -0,0 +1,38 @@
+module TetrisUI.Grid where
+import UI.NCurses
+
+block = "# "
+blocks n = concat $ replicate n block
+
+gridLines = 20
+gridColor = newColorID ColorBlue ColorDefault 1
+
+gridTop :: String
+gridTop = "____________________"
+
+gridMiddle :: String
+gridMiddle = "||  .   .   .   .   . ||"
+
+gridBottom :: String
+gridBottom = "+********************+"
+
+drawLines :: Integer -> Integer -> Update()
+drawLines y x = do drawLines' y x gridLines
+           where
+               drawLines' y x n | n > 0 = do
+                                moveCursor y x
+                                drawString gridMiddle
+                                drawLines' (y+1) x (n-1)
+                                | otherwise = return()
+
+
+drawGrid :: Integer -> Integer -> ColorID -> Update()
+drawGrid y x c = do
+        setColor c
+        moveCursor y (x+2)
+        drawString gridTop
+        drawLines (y+1) x
+        moveCursor (gridLines+y+1) (x+1)
+        drawString gridBottom
+
+
