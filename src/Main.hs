798778d949ef1a18cc6b76717c07b27661077852
Mihai Maruseac <mihai@leapyear.io>
2017-10-28 18:02:22 -0700

    Read and write scores to file

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,27 @@
+import System.Directory
 import TetrisCurses
 
 main :: IO()
-main = playGame [] >>= print
+main = do
+  scores <- unpersistScores
+  newScores <- playGame scores
+  persistScores newScores
+
+unpersistScores :: IO [Int]
+unpersistScores = do
+  hasF <- doesFileExist scoreFilePath
+  if not hasF then return [] else parseScores <$> readFile scoreFilePath
+
+persistScores :: [Int] -> IO ()
+persistScores [] = return ()
+persistScores scores = writeFile scoreFilePath (show scores)
+
+scoreFilePath :: FilePath
+scoreFilePath = "scores"
+
+parseScores :: String -> [Int]
+parseScores s
+  | null readses = []
+  | otherwise = fst . head $ readses
+  where
+    readses = reads s
